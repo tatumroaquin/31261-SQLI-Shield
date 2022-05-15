e81ba44f66389f4fda514c5c7cede1eb1820ef08
ormux <ormuseldesu@gmail.com>
2022-05-15 23:42:19 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center">31261 - SQLIA Proxy-based Detection</h1>
+<h5 align="center">15/05/2022</h5>
+
+<h3 align="center">Description</h3>
+
+A simple proxy designed as a proof of concept for the detection and prevention of SQLIA against a vulnerable web application hosted within the same network.
+
+<h3 align="center">Instructions</h3>
+
+1. create python virtual environment
+- `python -m venv venv`
+- `. venv/bin/activate`
+
+2. install requirements
+- `pip install -r requirements.txt`
+
+4. run the proxy
+- `python app.py [host] [port]`
+- `python app.py localhost 8000`
+
+<h4 align="center">technologies used</h4>
+<div align="center">
+   <img alt="flask" src="https://img.shields.io/badge/-Flask-black?logo=flask">
+   <img alt="python" src="https://img.shields.io/badge/-Python-black?logo=python&logoColor=yellow">
+</div>
---
+++ app.py
@@ -0,0 +1,57 @@
+import os, sys
+import argparse
+
+
+parser = argparse.ArgumentParser(description='SQLIA detection via reverse proxy', epilog='example: python '+os.path.basename(__file__)+' localhost 8000')
+parser.add_argument('host', metavar='host', type=str, help='ip address of the server')
+parser.add_argument('port', metavar='port', type=str, help='port number of the server')
+args = parser.parse_args()
+
+HOST = args.host
+PORT = args.port
+
+if HOST == '' or PORT == '':
+    print('please specify a server host and port')
+    sys.exit()
+
+SOCKET = "http://" + HOST + ":" + PORT + "/"
+CHARSET = ['"', "'", ';', '=', '-', '--']
+
+from flask import Flask, request, redirect, url_for
+from requests import get, put, post
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    title = b'<h1 align="center"> VIA PROXY </h1>'
+    payload = request.args.to_dict()
+    if sql_detect(payload): return redirect(url_for('sql_message'))
+    content = title + get(f'{SOCKET}', params=payload).content
+    return content
+
+@app.route('/<path:path>')
+def insert(path):
+    title = b'<h1 align="center"> VIA PROXY </h1>'
+    payload = request.args.to_dict()
+    if sql_detect(payload): return redirect(url_for('sql_message'))
+    content = title + get(f'{SOCKET}{path}', params=payload).content
+    return content
+
+@app.route('/injection')
+def sql_message():
+    return '<h1 align="center">SQL INJECTION DETECTED</h1>'
+
+def sql_detect(payload):
+    for key in payload:
+        for char in CHARSET:
+            current = payload[key]
+            if current.find(char) != -1:
+                return True
+    return False
+
+if __name__ == '__main__':
+    host = os.getenv("IP", "0.0.0.0")
+    port = int(os.getenv("PORT", 5000))
+    app.run(host=host, port=port, debug=False)
