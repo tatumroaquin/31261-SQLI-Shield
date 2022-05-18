65649263fd36d975c149f00f5b4c00b20fd2ab20
ormux <ormuseldesu@gmail.com>
2022-05-18 19:51:41 +1000

    use flask_ipban to block ip addresses attempt sql injection

--- app.py
+++ app.py
@@ -21,16 +21,23 @@ banner = figlet_format('SQLIA Guardian')
 print(banner)
 
 from flask import Flask, request, redirect, url_for
+from flask_ipban import IpBan
 from requests import get, put, post
 from markupsafe import escape
 
 app = Flask(__name__)
+ip_ban = IpBan()
+ip_ban.init_app(app)
+ip_ban.ban_seconds = 6
+ip_ban.ban_count = 1
 
 @app.route('/')
 def index():
     title = b'<h1 align="center"> VIA PROXY </h1>'
     payload = request.args.to_dict()
-    if sql_detect(payload): return redirect(url_for('sql_message'))
+    ip_addr = request.remote_addr
+    if sql_detect(payload, ip_addr): 
+        return redirect(url_for('sql_message'))
     content = title + get(f'{SOCKET}', params=payload).content
     return content
 
@@ -38,7 +45,9 @@ def index():
 def insert(path):
     title = b'<h1 align="center"> VIA PROXY </h1>'
     payload = request.args.to_dict()
-    if sql_detect(payload): return redirect(url_for('sql_message'))
+    ip_addr = request.remote_addr
+    if sql_detect(payload, ip_addr): 
+        return redirect(url_for('sql_message'))
     content = title + get(f'{SOCKET}{path}', params=payload).content
     return content
 
@@ -46,15 +55,16 @@ def insert(path):
 def sql_message():
     return '<h1 align="center">SQL INJECTION DETECTED</h1>'
 
-def sql_detect(payload):
+def sql_detect(payload, ip_addr):
     for key in payload:
         for token in TOKENS:
             current = payload[key]
             if current.find(token) != -1:
+                ip_ban.add(ip_addr)
                 return True
     return False
 
 if __name__ == '__main__':
-    host = os.getenv("IP", "0.0.0.0")
-    port = int(os.getenv("PORT", 5000))
+    host = '0.0.0.0'
+    port = '5000'
     app.run(host=host, port=port, debug=False)
