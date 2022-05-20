4428fe5d2107234135a19742a5402ea45645e00a
ormux <ormuseldesu@gmail.com>
2022-05-21 00:04:06 +1000

    add reboot dhcpcd

--- static-ip.sh
+++ static-ip.sh
@@ -5,13 +5,15 @@ rule="source-directory\s\/etc\/network\/interfaces.d"
 
 case $1 in
    on)
-   sudo echo "iface wlan0 inet static
-   address 192.168.0.105
-   netmask 255.255.255.0
-   gateway 192.168.0.1" >> $config
+sudo echo "iface wlan0 inet static
+address 192.168.0.105
+netmask 255.255.255.0
+gateway 192.168.0.1" >> $config
+sudo systemctl restart dhcpcd
       ;;
    off)
-      sudo cp $config temp.txt
-      sudo head -n -4 temp.txt > $config
-      sudo rm temp.txt
+sudo cp $config temp.txt
+sudo head -n -4 temp.txt > $config
+sudo rm temp.txt
+sudo systemctl restart dhcpcd
 esac
